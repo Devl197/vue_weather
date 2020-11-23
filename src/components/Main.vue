64e4ca9f8a73786286d9e4b6ecc19e57c31de9b2
Aleksandar Mladenovic <ml_aleksandar@hotmail.com>
2020-11-23 13:26:44 +0100

    Minor style changes to the Main component

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,6 +1,6 @@
 <template>
   <b-container>
-    <b-row class="rounded-lg shadow border">
+    <b-row>
       <b-col cols="12" lg="6" class="mt-2" id="weatherData">
         <h4>
           <b-icon-geo-alt-fill></b-icon-geo-alt-fill>{{ weatherData.name }},
@@ -20,9 +20,11 @@
           "
           alt="weather image"
         />
-        <h3 class="d-inline-block">{{ weatherData.current.temp }}&#176;C</h3>
+        <h3 class="d-inline-block">
+          {{ Math.round(weatherData.current.temp) }}&#176;C
+        </h3>
         <p>
-          Feels like {{ weatherData.hourly[0].feels_like }}&#176;C
+          Feels like {{ Math.round(weatherData.hourly[0].feels_like) }}&#176;C
           {{ weatherData.current.weather[0].description }}
         </p>
         <p>
@@ -30,24 +32,27 @@
           {{ weatherData.current.pressure }} hPa humidity:
           {{ weatherData.current.humidity }}% UV:
           {{ weatherData.current.uvi }} dew point:
-          {{ weatherData.current.dew_point }}&#176;C visibility:
+          {{ Math.round(weatherData.current.dew_point) }}&#176;C visibility:
           {{ weatherData.current.visibility / 1000 }}km
         </p>
       </b-col>
       <b-col cols="12" lg="6" class="mt-2" id="daily">
-        <p class="font-weight-bold text-center">8-day Forecast</p>
+        <h4 class="">8-day Forecast</h4>
         <b-row v-for="(daily, index) in weatherData.daily" :key="index">
           <b-col
-            cols="6"
-            class="d-flex align-items-center justify-content-lg-end justify-content-center"
-            ><p class="text-right">
+            cols="4"
+            class="d-flex align-items-center justify-content-start"
+            ><p class="d-sm-block d-none">
               {{ new Date(daily.dt * 1000).toDateString() }}
+            </p>
+            <p class="d-block d-sm-none">
+              {{ new Date(daily.dt * 1000).toLocaleDateString('en-GB') }}
             </p></b-col
           >
-          <b-col cols="6">
+          <b-col cols="8">
             <p>
               <img
-                class="text-center d-none d-md-inline-block"
+                class="text-center d-none d-sm-inline-block"
                 :src="
                   `http://openweathermap.org/img/wn/${daily.weather[0].icon}@2x.png`
                 "
@@ -55,7 +60,9 @@
                 width="40"
                 height="40"
               />
-              {{ daily.temp.min }}/{{ daily.temp.max }}°C
+              {{ Math.round(daily.temp.max) }}/{{
+                Math.round(daily.temp.min)
+              }}°C
               {{ daily.weather[0].description }}
             </p>
           </b-col>
@@ -66,33 +73,33 @@
 </template>
 
 <script>
-export default {
-  name: 'Main',
-  props: {
-    weatherData: {},
-  },
-  methods: {
-    returnTimeString() {
-      const date = new Date(this.weatherData.current.dt * 1000);
-      const options = {
-        weekday: 'long',
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric',
-      };
-      return `${date.toLocaleTimeString('en-GB')} ${date.toLocaleDateString(
-        'en-GB',
-        options
-      )}`;
+  export default {
+    name: 'Main',
+    props: {
+      weatherData: {},
     },
-  },
-};
+    methods: {
+      returnTimeString() {
+        const date = new Date(this.weatherData.current.dt * 1000);
+        const options = {
+          weekday: 'long',
+          year: 'numeric',
+          month: 'long',
+          day: 'numeric',
+        };
+        return `${date.toLocaleTimeString('en-GB')} ${date.toLocaleDateString(
+          'en-GB',
+          options
+        )}`;
+      },
+    },
+  };
 </script>
 
 <style lang="scss" scoped>
-#daily {
-  p {
-    margin-bottom: 5px;
+  #daily {
+    p {
+      margin-bottom: 5px;
+    }
   }
-}
 </style>
